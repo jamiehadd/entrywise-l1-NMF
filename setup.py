e570e2878bb07272fe5710bfdbdc0aaeace820e0
kunyanglu <lualicia88@gmail.com>
2022-09-08 20:22:07 -0700

    update setup

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from setuptools import setup
 
 setup(
-        name='entrywise-L1-NMF',
+        name='entrywise-l1-NMF',
         version='0.1',
-        description="Entrywise L1-Norm Non-Negative Matrix Factorization',
+        description="Entrywise L1-Norm Non-Negative Matrix Factorization",
         url='https://github.com/jamiehadd/entrywise-l1-NMF',
         author='Toby Anderson, Jamie Haddock, Alicia Lu',
         author_email='allu@g.hmc.edu, tobanderson@g.hmc.edu',
         license='MIT',
         packages=[
-            'entrywise-L1-NMF'
+            'entrywise-l1-NMF'
         ],
         install_requires=[
             'numpy'
